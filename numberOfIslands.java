e4620e6bae75775bf660ed4228d463efb50fbed9
Nick McCullough <91184284+mccnick@users.noreply.github.com>
2023-04-30 08:26:11 -0500

    Add files via upload

---
+++ numberOfIslands.java
@@ -0,0 +1,119 @@
+package leetcodePractice;
+
+public class numberOfIslands
+{
+
+	/*
+	 * *****MEDIUM*****
+	 * 
+	 * Given an m x n 2D binary grid grid which represents a map of '1's (land) and
+	 * '0's (water), return the number of islands.
+	 * 
+	 * An island is surrounded by water and is formed by connecting adjacent lands
+	 * horizontally or vertically. You may assume all four edges of the grid are all
+	 * surrounded by water.
+	 * 
+	 * Constraints:
+	 * 
+	 * m == grid.length
+	 * 
+	 * n == grid[i].length
+	 * 
+	 * 1 <= m, n <= 300
+	 * 
+	 * grid[i][j] is '0' or '1'.
+	 * 
+	 */
+
+	public int numIslands(char[][] grid)
+	{
+		int islands = 0;
+
+		// iterate thru the width and height of 2D array
+		for (int i = 0; i < grid.length; i++)
+		{
+			for (int j = 0; j < grid[i].length; j++)
+			{
+
+				if (grid[i][j] == '1') // char 2D array, not INT!!
+				{
+					// found a 1, increment island
+					islands++;
+
+					// create a breadth first search recursive method to call this
+					callBFS(grid, i, j);
+
+				}
+
+			}
+		}
+		return islands;
+	}
+
+	private void callBFS(char[][] grid, int i, int j)
+	{
+		// boundary check first
+		if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0')
+		{
+			return; // breaks out of recursion
+		}
+
+		// set any additional 1's to a 0 for now, remember not int, CHAR!!!
+		grid[i][j] = '0'; 
+
+		// call the method for up, down, left, right
+		callBFS(grid, i + 1, j); // up
+		callBFS(grid, i - 1, j); // down
+		callBFS(grid, i, j - 1); // left
+		callBFS(grid, i, j + 1); // right
+	}
+
+	// -------------------- chatGPT QUICK TESTS --------------------
+
+	public static void main(String[] args)
+	{
+		numberOfIslands solution = new numberOfIslands();
+
+		char[][] grid1 = { { '1', '1', '1', '1', '0' }, { '1', '1', '0', '1', '0' }, { '1', '1', '0', '0', '0' },
+				{ '0', '0', '0', '0', '0' } };
+		int expected1 = 1;
+		int actual1 = solution.numIslands(grid1);
+		System.out.println("Test case 1 - Expected: " + expected1 + ", Actual: " + actual1);
+
+		char[][] grid2 = { { '1', '1', '0', '0', '0' }, { '1', '1', '0', '0', '0' }, { '0', '0', '1', '0', '0' },
+				{ '0', '0', '0', '1', '1' } };
+		int expected2 = 3;
+		int actual2 = solution.numIslands(grid2);
+		System.out.println("Test case 2 - Expected: " + expected2 + ", Actual: " + actual2);
+
+		char[][] grid3 = { { '1', '1', '1' }, { '0', '1', '0' }, { '1', '1', '1' } };
+		int expected3 = 1;
+		int actual3 = solution.numIslands(grid3);
+		System.out.println("Test case 3 - Expected: " + expected3 + ", Actual: " + actual3);
+
+		char[][] grid4 = { { '1', '1', '1', '1', '1' }, { '1', '1', '1', '1', '1' }, { '1', '1', '1', '1', '1' },
+				{ '1', '1', '1', '1', '1' }, { '1', '1', '1', '1', '1' } };
+		int expected4 = 1;
+		int actual4 = solution.numIslands(grid4);
+		System.out.println("Test case 4 - Expected: " + expected4 + ", Actual: " + actual4);
+
+		char[][] grid5 = { { '0', '0', '0', '0', '0' }, { '0', '0', '0', '0', '0' }, { '0', '0', '0', '0', '0' },
+				{ '0', '0', '0', '0', '0' } };
+		int expected5 = 0;
+		int actual5 = solution.numIslands(grid5);
+		System.out.println("Test case 5 - Expected: " + expected5 + ", Actual: " + actual5);
+	}
+
+	/*
+	 * CONSOLE OUTPUT:
+	 * 
+	 * Test case 1 - Expected: 1, Actual: 1
+	 * Test case 2 - Expected: 3, Actual: 3
+	 * Test case 3 - Expected: 1, Actual: 1
+	 * Test case 4 - Expected: 1, Actual: 1
+	 * Test case 5 - Expected: 0, Actual: 0
+	 * 
+	 * it works, done
+	 */
+
+}
---
+++ twoSum.java
@@ -0,0 +1,60 @@
+package leetcodePractice;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target. - You may assume that each input
+ * would have exactly one solution, and you may not use the same element twice.
+ * - You can return the answer in any order.
+ */
+
+/*
+ * Steps to solve.
+ * 
+ * Understand the problem: The Two Sum problem asks us to find two numbers in an array that add up to a given target value.
+ * 
+ * Plan the solution: To solve the problem, we need to iterate through the array and for each element, 
+ * check if the difference between the target value and the current element exists in the array.
+ * If it does, we have found the two numbers that add up to the target value.
+ */
+
+public class twoSum
+{
+	public static int[] twoSumSolution(int[] nums, int target)
+	{
+		Map<Integer, Integer> map = new HashMap<>(); // new hashmap
+		for (int i = 0; i < nums.length; i++) // iterate length of nums array
+		{
+			int complement = target - nums[i]; // new variable
+			if (map.containsKey(complement))
+			{
+				return new int[] { map.get(complement), i }; // return new int complement index in array and the iteration i
+			}
+			map.put(nums[i], i); // map doesnt contain complement, add nums[i] and i to map
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
+
+	// test cases
+
+	public static void main(String[] args)
+	{
+		int[] nums1 = { 2, 7, 11, 15 };
+		int target1 = 9;
+		int[] result1 = twoSumSolution(nums1, target1); // run method with these variables
+		System.out.println(Arrays.toString(result1)); // [0, 1] 2 + 7 add up to 9
+
+		int[] nums2 = { 3, 2, 4 };
+		int target2 = 6;
+		int[] result2 = twoSumSolution(nums2, target2); // run method with these variables
+		System.out.println(Arrays.toString(result2)); // [1, 2] 2 + 4 add up to 6
+
+		int[] nums3 = { 3, 3 };
+		int target3 = 6;
+		int[] result3 = twoSumSolution(nums3, target3); // run method with these variables
+		System.out.println(Arrays.toString(result3)); // [0, 1] 3 + 3 add up to 6 etc
+	}
+}
